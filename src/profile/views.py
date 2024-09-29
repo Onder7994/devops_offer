e276a994d9991c3016ca99067f03d791f1602f10
Onder7994 <andrei.trusikhin@gmail.com>
2024-09-29 20:25:19 +0300

    Add profile edit

--- src/profile/views.py
+++ src/profile/views.py
@@ -1,10 +1,16 @@
 from typing import Annotated, List, Sequence
 
-from fastapi import APIRouter, Depends, Request, status
+from fastapi import APIRouter, Depends, Request, status, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from fastapi_users.exceptions import UserAlreadyExists, InvalidPasswordException
+from pydantic import ValidationError
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.auth.dependencies import get_user_manager
+from src.auth.manager import UserManager
+from src.auth.schemas import UserUpdate
+from .schemas import EditProfileForm
 from src.category.models import Category
 from src.common.dependencies import get_categories
 from src.config import settings
@@ -13,6 +19,7 @@ from src.auth.models import User
 from src.db.database import db_helper
 from src.favorite.dependencies import get_user_favorites
 from starlette.responses import RedirectResponse
+from src.utils.utils import get_hashed_password, verify_password
 
 templates = Jinja2Templates(directory="templates")
 
@@ -42,7 +49,7 @@ async def profile(
 
 
 @router.get("/edit", response_class=HTMLResponse)
-async def edit_profile(
+async def edit_profile_form(
     request: Request,
     user: Annotated[User, Depends(current_active_user_ui)],
     categories: Sequence[Category] = Depends(get_categories),
@@ -57,4 +64,104 @@ async def edit_profile(
             "user": user,
             "categories": categories,
         },
+    )
+
+
+@router.post("/edit", response_class=HTMLResponse)
+async def edit_profile(
+    request: Request,
+    user: Annotated[User, Depends(current_active_user_ui)],
+    user_manager: Annotated[UserManager, Depends(get_user_manager)],
+    categories: Sequence[Category] = Depends(get_categories),
+    username: str = Form(...),
+    email: str = Form(...),
+    current_password: str = Form(...),
+    new_password: str = Form(...),
+):
+    errors = []
+    try:
+        form = EditProfileForm(
+            username=username,
+            email=email,
+            current_password=current_password,
+            new_password=new_password,
+        )
+    except ValidationError as err:
+        errors_messages = [err["msg"] for err in err.errors()]
+        return templates.TemplateResponse(
+            "auth/edit_profile.html",
+            {
+                "request": request,
+                "errors": errors_messages,
+                "categories": categories,
+                "user": user,
+            },
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+
+    if not verify_password(form.current_password, user.hashed_password):
+        errors.append("Текущий пароль введен неправильно.")
+
+    if form.email != user.email:
+        is_user_exist = await user_manager.get_by_email(user_email=form.email)
+        if is_user_exist:
+            errors.append("Пользователь с таким email уже существует")
+
+    if errors:
+        return templates.TemplateResponse(
+            "auth/edit_profile.html",
+            {
+                "request": request,
+                "errors": errors,
+                "user": user,
+                "categories": categories,
+            },
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+
+    try:
+        user_update = UserUpdate(
+            username=form.username,
+            email=form.email,
+            password=form.new_password,
+        )
+        user = await user_manager.update(
+            user_update=user_update, user=user, request=request
+        )
+
+    except InvalidPasswordException as err:
+        return templates.TemplateResponse(
+            "auth/edit_profile.html",
+            {
+                "request": request,
+                "errors": [str(err)],
+                "categories": categories,
+                "user": user,
+            },
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+
+    except Exception:
+        return templates.TemplateResponse(
+            "auth/edit_profile.html",
+            {
+                "request": request,
+                "errors": [
+                    "Произошла неизвестная ошибка. Пожалуйста, попробуйте позже"
+                ],
+                "categories": categories,
+                "user": user,
+            },
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+
+    return templates.TemplateResponse(
+        "auth/edit_profile.html",
+        {
+            "request": request,
+            "user": user,
+            "categories": categories,
+            "success": True,
+        },
+        status_code=status.HTTP_200_OK,
     )
